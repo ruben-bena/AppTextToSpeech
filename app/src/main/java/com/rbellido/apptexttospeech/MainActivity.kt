76bec64e87d34ce9282943fb96f27464a5ac3961
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2026-01-19 16:34:38 +0100

    MainActivity finished

--- app/src/main/java/com/rbellido/apptexttospeech/MainActivity.kt
+++ app/src/main/java/com/rbellido/apptexttospeech/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.rbellido.apptexttospeech
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.rbellido.apptexttospeech.databinding.ActivityMainBinding
+import java.util.Locale
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var tts: TextToSpeech
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        tts = TextToSpeech(this, this)
+
+        binding.button.setOnClickListener {
+            val toSpeak = binding.editText.text.toString()
+            tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, null)
+        }
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            tts.language = Locale("es", "ES")
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tts.stop()
+        tts.shutdown()
     }
 }
